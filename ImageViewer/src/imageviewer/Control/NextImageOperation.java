5eaa25d8ba3cbadba0780d85352ffde1e7793723
David <David@192.168.1.37>
2015-01-04 14:18:24 +0000

    create package control with operations

--- ImageViewer/src/imageviewer/Control/NextImageOperation.java
+++ ImageViewer/src/imageviewer/Control/NextImageOperation.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package imageviewer.Control;
 
-/**
- *
- * @author David
- */
-public class NextImageOperation {
+public class NextImageOperation implements Operation{
+
+    @Override
+    public void execute() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- ImageViewer/src/imageviewer/Control/PrevImageOperation.java
+++ ImageViewer/src/imageviewer/Control/PrevImageOperation.java
@@ -1,15 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package imageviewer.Control;
 
-/**
- *
- * @author David
- */
-public class PrevImageOperation {
+public class PrevImageOperation implements Operation{
+
+    @Override
+    public void execute() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
