366e1e8c761a9a5519c57cd647b407d5cb36a8cb
David <David@192.168.1.37>
2015-01-04 12:59:00 +0000

    Implemented interface BITMAP

--- ImageViewer/src/imageviewer/Model/BitMap.java
+++ ImageViewer/src/imageviewer/Model/BitMap.java
@@ -1,15 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package imageviewer.Model;
 
-/**
- *
- * @author David
- */
-public class BitMap {
-    
+public interface BitMap {
+    public int getWidth();
+    public int getHeight();   
 }
