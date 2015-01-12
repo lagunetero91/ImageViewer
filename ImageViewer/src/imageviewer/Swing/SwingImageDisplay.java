7a0dee18202c19387f5770d8bc4c495ab03747e1
David <David@192.168.1.37>
2015-01-12 16:55:55 +0000

    SwingImageDisplay implemented(but no botton work)

--- ImageViewer/src/imageviewer/Swing/SwingImageDisplay.java
+++ ImageViewer/src/imageviewer/Swing/SwingImageDisplay.java
@@ -2,14 +2,19 @@ package imageviewer.Swing;
 
 import imageviewer.Model.Image;
 import imageviewer.UI.ImageDisplay;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import static java.time.Clock.offset;
 import javax.swing.JPanel;
 
 public class SwingImageDisplay extends JPanel implements ImageDisplay{
     private Image image;
+    private int offset;
     
     @Override
     public void setImage(Image image) {
         this.image=image;
+       repaint();
     }
 
     @Override
@@ -17,4 +22,20 @@ public class SwingImageDisplay extends JPanel implements ImageDisplay{
         return image;
     }
     
+    public BufferedImage getBitmap(){
+        if(image.getBitMap() instanceof SwingBitMap)
+            return getBufferedImage((SwingBitMap) image.getBitMap());
+        return null;
+    }
+
+    private BufferedImage getBufferedImage(SwingBitMap swingBitMap) {
+        return swingBitMap.getBufferdedImage();
+    }
+
+    public void paint (Graphics g){
+        super.paint(g);
+        g.drawImage(getBitmap(), offset, 0, null);
+        
+    }
 }
+
